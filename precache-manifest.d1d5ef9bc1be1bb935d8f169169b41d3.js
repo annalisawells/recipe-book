1eba8a7d5ab5e6afed19a41733bd72f1718cdf49
annalisawells <annalisawells@gmail.com>
2019-10-21 17:03:59 -0600

    Updates

---
+++ precache-manifest.d1d5ef9bc1be1bb935d8f169169b41d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99abc3144fe74d0f1af373983520e7e2",
+    "url": "/recipe-book/index.html"
+  },
+  {
+    "revision": "c4ab3dabe7ab955159c8",
+    "url": "/recipe-book/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "75decbaf01f81541593d",
+    "url": "/recipe-book/static/js/2.e1df02c2.chunk.js"
+  },
+  {
+    "revision": "c4ab3dabe7ab955159c8",
+    "url": "/recipe-book/static/js/main.77ab3436.chunk.js"
+  },
+  {
+    "revision": "eea79a336f67352cfe56",
+    "url": "/recipe-book/static/js/runtime-main.ac34b0da.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/recipe-book/static/media/logo.25bf045c.svg"
+  }
+]);
